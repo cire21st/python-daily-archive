08948bebd518b5cf06f610f869f975813b103616
Eric Park <79054343+cire21st@users.noreply.github.com>
2026-09-14 22:33:53 +0900

    Update 피자 나눠 먹기 (3).py

--- Lv0/피자 나눠 먹기 (3).py
+++ Lv0/피자 나눠 먹기 (3).py
@@ -1,5 +1,7 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/120816
 def solution(slice, n):
     pizza = 0
-    if n >= (pizza * slice): return pizza
-    else: pizza += 1
+    while(n > (pizza * slice)):
+        if n > (pizza * slice): pizza += 1 
+        else: break 
+    return pizza
