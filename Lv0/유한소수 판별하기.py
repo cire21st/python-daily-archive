d16722c8ebf01129f8f26d37681e423b2500704a
Eric Park <79054343+cire21st@users.noreply.github.com>
2026-09-16 10:42:39 +0900

    Update 유한소수 판별하기.py

--- Lv0/유한소수 판별하기.py
+++ Lv0/유한소수 판별하기.py
@@ -1,6 +1,12 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/120878
 def solution(a, b):
 #두 값을 소인수분해
+while True:
+    prime = [2,3,5,7....]
+    for i in prime
+        if a%i == 0 and b%i == 0:
+            a = a/number
+            b = b/number
+        elif prime[-1] = i: break
 #두 값의 최대공약수를 찾음
 #최대공약수로 a,b를 각각 나눔
 #b의 소인수에 2,5외에 다른것이 있으면 return 2
