53406fdd3604c59020623493f63f6ff0f0c6e764
Eric Park <79054343+cire21st@users.noreply.github.com>
2026-09-17 17:11:14 +0900

    Update 배열 뒤집기.py

--- Lv0/배열 뒤집기.py
+++ Lv0/배열 뒤집기.py
@@ -1,6 +1,4 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/120821
 def solution(num_list):
-    return_list = []
-    for i in range(1,len(num_list)+1):
-        return_list.append(num_list[-i])
-    return return_list
+    num_list.reverse()
+    return num_list
