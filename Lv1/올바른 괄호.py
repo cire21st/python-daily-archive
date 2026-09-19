e4c2c14ebc1b7e365e22126e5cc900da5a8c324b
Eric Park <79054343+cire21st@users.noreply.github.com>
2026-09-19 18:58:51 +0900

    Create 올바른 괄호.py

---
+++ Lv1/올바른 괄호.py
@@ -0,0 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12909#
+def solution(s):
+    cnt = 0
+    if s[0] == '(' and s[-1] == ')':
+        for i in s:
+            if cnt == 0 and i == ')': return False
+            cnt+= 1 if i == '(' else -1
+        if cnt == 0: return True
+    return False
