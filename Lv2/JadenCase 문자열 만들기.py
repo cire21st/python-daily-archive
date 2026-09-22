9ee41dc88e96cd238ecd30d9f2bcdb81265d8c62
Eric Park <79054343+cire21st@users.noreply.github.com>
2026-09-22 20:07:55 +0900

    Update JadenCase 문자열 만들기.py

--- Lv2/JadenCase 문자열 만들기.py
+++ Lv2/JadenCase 문자열 만들기.py
@@ -1,12 +1,11 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12951
 def solution(s):
-    #split으로 단어를 분절함
-    #Upper을 [0]에 대해서만, 나머지는 lower 실행
-    cnt = []
+    #문자열을 순회함
+    #공백은 공백, 공백이후 문자는 대문자, 문자or숫자이후 문자는 소문자
+    result = []
     for i in range(len(s)):
-        if s[i] == ' ': cnt.append(i)
-    s = s.split()
-    answer = []
-    for i in s:
-        answer.append(i[0].upper() + i[1:].lower())
-    return ' '.join(answer)
+        if s[i] == ' ': result.append(' ')
+        elif i == 0 or s[i-1] == ' ': result.append(s[i].upper())
+        else :result.append(s[i].lower())
+
+    return ''.join(result)
